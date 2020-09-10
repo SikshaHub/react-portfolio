5432adf514bffa8a5f2f4b985b90780f4911b23d
sikshahub <sikshahub@gmail.com>
2020-09-11 01:17:01 +0530

    Updates

---
+++ precache-manifest.d4e60019d01686109ee83b7ee0d3deac.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b11b48fdbb8977afd4f5329e7c528e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbe25bafd4361855f89f",
+    "url": "/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "d56fb464cbfd911690a2",
+    "url": "/static/css/main.690c587f.chunk.css"
+  },
+  {
+    "revision": "fbe25bafd4361855f89f",
+    "url": "/static/js/2.6ea13cbf.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/static/js/2.6ea13cbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d56fb464cbfd911690a2",
+    "url": "/static/js/main.178e6128.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/static/media/Roboto-Thin.321de678.ttf"
+  },
+  {
+    "revision": "ed1d014bf2b8b72f27d6af65c69f710a",
+    "url": "/static/media/TitilliumWeb-Regular.ed1d014b.ttf"
+  },
+  {
+    "revision": "e00e2a77dd88a8fe75573a5d993af76a",
+    "url": "/static/media/Ubuntu-Bold.e00e2a77.ttf"
+  },
+  {
+    "revision": "4b96047e4af086277cdaeb9e60857534",
+    "url": "/static/media/Ubuntu-Italic.4b96047e.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/static/media/Ubuntu-Regular.2505bfbd.ttf"
+  },
+  {
+    "revision": "f956d6dd53245ac2cfe24ba3ec21e90e",
+    "url": "/static/media/chemistry.f956d6dd.jpg"
+  },
+  {
+    "revision": "bf5c1c7f5e1d82ef37ac524c721df754",
+    "url": "/static/media/current_affairs.bf5c1c7f.jpg"
+  },
+  {
+    "revision": "a6b07946691bc88f5a6282b4f6e38d89",
+    "url": "/static/media/education.a6b07946.svg"
+  },
+  {
+    "revision": "3bee7566d94381663826fd422c14796e",
+    "url": "/static/media/environmental.3bee7566.jpg"
+  },
+  {
+    "revision": "c5c255aea3bc3007b13db45bd496b7a2",
+    "url": "/static/media/geography.c5c255ae.jpg"
+  },
+  {
+    "revision": "c0fa3517f58ca9c01c0739d40b704969",
+    "url": "/static/media/history.c0fa3517.jpg"
+  },
+  {
+    "revision": "17cf6af1248f1294a938f34e918e17c0",
+    "url": "/static/media/physics.17cf6af1.jpg"
+  },
+  {
+    "revision": "694881d8b23442d7952ad1ebac248644",
+    "url": "/static/media/school.694881d8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.219ad60d54e51800e67fe4dabbf4aeb1.js"
+  "/precache-manifest.d4e60019d01686109ee83b7ee0d3deac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
